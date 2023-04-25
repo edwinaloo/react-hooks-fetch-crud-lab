39f4552404a4dd5e25954165b718df95ab7fcf67
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-25 07:50:30 +0300

    Question form fix and update

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,23 +1,4 @@
 import React, { useState, useEffect } from "react";
-import QuestionList from "./QuestionList";
-
-function App() {
-  const [questions, setQuestions] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/questions-060")
-      .then((response) => response.json())
-      .then((data) => setQuestions(data))
-      .catch((error) => console.log(error));
-  }, []);
-
-  return (
-    <div className="App">
-      <QuestionList questions={questions} />
-    </div>
-  );
-}
-import { useState, useEffect } from 'react';
 
 function QuestionList() {
   const [questions, setQuestions] = useState([]);
@@ -29,12 +10,27 @@ function QuestionList() {
       .catch(error => console.error(error));
   }, []);
 
-  const handleDelete = (id) => {
+  const handleDropdownChange = (id, value) => {
     fetch(`/questions/${id}`, {
-      method: 'DELETE'
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ correctIndex: value })
     })
       .then(() => {
-        // Remove the deleted question from the state
+        const updatedQuestions = questions.map(question => {
+          if (question.id === id) {
+            return { ...question, correctIndex: value };
+          }
+          return question;
+        });
+        setQuestions(updatedQuestions);
+      })
+      .catch(error => console.error(error));
+  };
+
+  const handleDelete = (id) => {
+    fetch(`/questions/${id}`, { method: 'DELETE' })
+      .then(() => {
         const updatedQuestions = questions.filter(question => question.id !== id);
         setQuestions(updatedQuestions);
       })
@@ -58,6 +54,23 @@ function QuestionList() {
         ))}
       </ul>
     </>
+  );
+}
+
+function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("/questions-060")
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <QuestionList questions={questions} />
+    </div>
   );
 }
 
