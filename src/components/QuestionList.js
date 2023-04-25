7b1d4daeab31b435dfbd88881d6405909a38bd43
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-04-25 07:47:50 +0300

    add delete button

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -17,5 +17,48 @@ function App() {
     </div>
   );
 }
+import { useState, useEffect } from 'react';
+
+function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('/questions')
+      .then(response => response.json())
+      .then(data => setQuestions(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch(`/questions/${id}`, {
+      method: 'DELETE'
+    })
+      .then(() => {
+        // Remove the deleted question from the state
+        const updatedQuestions = questions.filter(question => question.id !== id);
+        setQuestions(updatedQuestions);
+      })
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <>
+      <h2>Questions</h2>
+      <ul>
+        {questions.map(question => (
+          <li key={question.id}>
+            <p>{question.prompt}</p>
+            <ul>
+              {question.answers.map((answer, index) => (
+                <li key={index}>{answer}</li>
+              ))}
+            </ul>
+            <button onClick={() => handleDelete(question.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 export default App;
